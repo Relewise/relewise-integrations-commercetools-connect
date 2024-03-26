8cd945a9f305f67396b0ffb0b6b298d515bb9935
Martin Zanoni <mza@relewise.com>
2024-03-26 11:29:37 +0100

    working test baseline

--- full-sync/tests/mapping/helpers.spec.ts
+++ full-sync/tests/mapping/helpers.spec.ts
@@ -1,10 +1,47 @@
 import { describe, expect, test } from '@jest/globals';
-import { localizedToMultilingual } from '../src/mapping/helpers';
+import { localizedToMultilingual } from '../../src/mapping/helpers';
+import { DataValueFactory } from '@relewise/client';
 
 describe('Testing helpers', () => {
-  test('localizedToMultilingual null value', () => {
+    test('localizedToMultilingual null value', () => {
 
-    localizedToMultilingual(null)
-    expect(1).toBe(1);
-  });
+        const result = localizedToMultilingual(undefined)
+        expect(result).toBe(null);
+    });
 });
+
+describe('Testing helpers', () => {
+    test('localizedToMultilingual 1 language', () => {
+
+        const subject = localizedToMultilingual({
+            "en": "test"
+        });
+
+        expect(subject).toStrictEqual(DataValueFactory.multilingual([{
+            language: "en",
+            value: "test"
+        }]));
+    });
+});
+
+describe('Testing helpers', () => {
+    test('localizedToMultilingual 2 languages', () => {
+
+        const subject = localizedToMultilingual({
+            "en": "test",
+            "da": "test2",
+        });
+
+        expect(subject).toStrictEqual(DataValueFactory.multilingual([
+            {
+                language: "en",
+                value: "test"
+            },
+            {
+                language: "da",
+                value: "test2"
+            }
+        ]));
+    });
+});
+
